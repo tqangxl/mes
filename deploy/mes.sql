9ed2ae2ba05fea9b7722a72bbf12f3e15f33b3bd
Edoardo Spadoni <edoardo.spadoni@gmail.com>
2017-12-18 21:35:38 +0100

    db added new tables

--- deploy/mes.sql
+++ deploy/mes.sql
@@ -1,8 +1,26 @@
-CREATE DATABASE IF NOT EXISTS mes;
+CREATE DATABASE IF NOT EXISTS `mes`;
 
-CREATE TABLE IF NOT EXISTS models (
-    id serial,
-    code varchar(50),
-    name varchar(200),
-    PRIMARY KEY (id)
+CREATE TABLE IF NOT EXISTS `mes`.`articles` (
+    `id` serial,
+    `code` varchar(50),
+    `description` varchar(200),
+    `pz_container` int,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `mes`.`models` (
+    `id` serial,
+    `code` varchar(50),
+    `description` varchar(200),
+    `article_id` bigint unsigned,
+    FOREIGN KEY (`article_id`) REFERENCES articles(`id`),
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `mes`.`params` (
+    `id` serial,
+    `code` varchar(50),
+    `description` varchar(200),
+    `type` varchar(200),
+    PRIMARY KEY (`id`)
 );
